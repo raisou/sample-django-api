608b1b708a1bc259d860ecb1f3cf239ee07b35ff
Fabien Loffredo <contact@fabienloffredo.com>
2020-12-09 18:50:12 +0100

    Update manage.py to use our settings format

--- manage.py
+++ manage.py
@@ -2,20 +2,30 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_api.settings')
+    local_file = Path("sample_api/settings/local.py")
+    if local_file.is_file():
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "sample_api.settings.local")
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "sample_api.settings.production")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  # noqa
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    try:
+        execute_from_command_line(sys.argv)
+    except ImportError as e:
+        print(e)
 
 
 if __name__ == '__main__':
