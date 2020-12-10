228a689476bed6d43628c6cf9063cf2cab8c5d91
Fabien Loffredo <contact@fabienloffredo.com>
2020-12-10 09:47:04 +0100

    Docs - Use drf-spectacular instead of drf-yasg

--- sample_api/urls.py
+++ sample_api/urls.py
@@ -1,35 +1,18 @@
 from django.contrib import admin
-from django.urls import (path, include, re_path)
+from django.urls import (path, include)
+from django.views.generic import RedirectView
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView)
 
 from . import api_urls
 
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Sample API",
-      default_version='v1',
-      description="Sample prototyping API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@fabienloffredo.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
-
-
 urlpatterns = [
-    re_path(
-        r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(
-        r'^swagger/$',
-        schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(
-        r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', RedirectView.as_view(url="/docs/swagger/"), name='docs'),
+    path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('admin/', admin.site.urls),
     path('api/', include(api_urls), name="api")
