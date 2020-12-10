d4eccc14ae7d955fa7a0b7c375ef2a9f72ca012b
Fabien Loffredo <contact@fabienloffredo.com>
2020-12-10 21:41:50 +0100

    Unallow to create celery task in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django_celery_results.admin import TaskResultAdmin
 
-from .models import Task
+from .models import (Task, CeleryTask)
+
+
+admin.site.unregister(Group)
+admin.site.unregister(CeleryTask)
+
+
+@admin.register(CeleryTask)
+class CeleryTaskAdmin(TaskResultAdmin):
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task_type', 'celery_task', 'user')
     list_per_page = 20
-
-
-admin.site.unregister(Group)
