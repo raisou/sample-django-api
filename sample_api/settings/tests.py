ebb548813f9757e1922e60096e0647d9e7780953
Fabien Loffredo <contact@fabienloffredo.com>
2020-12-11 18:31:36 +0100

    Add initial test context

---
+++ sample_api/settings/tests.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+
+from .defaults import *  # noqa
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travis_ci',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+SECRET_KEY = 'zj6ay3l7s^ga3!*!zi(s81+jj01eblt=wxzzd3=__n0wh0a5uw'
+
+LANGUAGE_CODE = "fr-FR"
+
+TIME_ZONE = 'Europe/Paris'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
+
+MEDIA_URL = "/media/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ tasks/tests.py
@@ -0,0 +1,23 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from sample_api.factories import UserFactory
+
+
+class TaskAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.user = UserFactory.create()
+
+    def test_anonymous_list_tasks(self):
+        response = self.client.get(reverse('tasks-list'))
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_list_tasks(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(reverse('tasks-list'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
